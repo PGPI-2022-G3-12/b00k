2317c65b79a376b2494ede93564e8bd8a8dd6400
IJordano <n.jordano10@gmail.com>
2022-11-26 20:57:50 +0100

    feat: Add basic models in store app

--- b00k/store/models.py
+++ b00k/store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    price = models.FloatField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book)

bba66650e106630369bdafa98935114be8abb845
IJordano <n.jordano10@gmail.com>
2022-11-27 19:04:21 +0100

    feat: Add catalog view

--- b00k/store/views.py
+++ b00k/store/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from .models import Book, Category
 
 # Create your views here.
 
 def index(request):
     # Escaparate
-    return render(request, 'index.html')
+    categoryList = Category.objects.order_by('name')
+    displayCategoryList = Category.objects.order_by('name')[:6] # Order by most popular? - pending sales model
+
+    context = {
+        'categoryList': categoryList,
+        'displayCategoryList': displayCategoryList,
+    }
+
+    return render(request, 'index.html', context)
 
 def catalog(request):
-    pass
+    # Catálogo
+    bookOrder = request.GET.get('orderBy', 'title')
+    nProducts = request.GET.get('nProducts', 1)
+    pageNumber = request.GET.get('page', 1)
+
+    categoryList = Category.objects.order_by('name')
+    bookList = Book.objects.order_by(bookOrder)
+    paginator = Paginator(bookList, nProducts)
+    pageObj = paginator.get_page(pageNumber)
+
+    context = {
+        'categoryList': categoryList,
+        'bookList': pageObj,
+        'pageNumber': pageNumber,
+        'nProducts': nProducts,
+        'pageObj': pageObj,
+    }
+
+    return render(request, 'catalog.html', context)

c7a4b689ac6f0825af83747e6d377681d511cd0f
JuanJoCasamitjana <73534780+JuanJoCasamitjana@users.noreply.github.com>
2022-11-29 17:12:06 +0100

    Gestion de clientes

--- b00k/store/urls.py
+++ b00k/store/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('clients',views.clients, name='clients'),
+    path('signup',views.signup, name='signup'),
+    path('signin',views.signin, name='signin')
 ]
--- b00k/store/views.py
+++ b00k/store/views.py
@@ -8,3 +8,12 @@ def index(request):
 
 def catalog(request):
     pass
+
+def clients(request):
+    return render(request,'clients.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+def signin(request):
+    return render(request,'signin.html')

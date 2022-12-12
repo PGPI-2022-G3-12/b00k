af45c78362f3fe2ce010d348efc9c59ba1f61078
vicgrabru <vgravanbru@gmail.com>
2022-12-12 13:12:37 +0100

    Añadidos datos de contacto al pedido

--- b00k/store/models.py
+++ b00k/store/models.py
@@ -11,8 +11,14 @@ class Category(models.Model):
 
 class Cart(models.Model):
     client = models.CharField(max_length=255, unique=True)
+    firstName = models.CharField(max_length=150, default='Jane')
+    lastName = models.CharField(max_length=150, default='Doe')
+    email = models.EmailField(max_length=254, unique=True, blank=True)
+    address = models.CharField(max_length=255, blank=False, null=True)
     books = models.ManyToManyField(Book)
-    
+    totalPrice = models.FloatField(default=0.0)
+
+
     # def get_price(self):
     #     price = self.books.aggregate(final_price=models.Sum('price'))
     #     result = price["final_price"]
@@ -20,17 +26,23 @@ class Cart(models.Model):
     #         return 0.0
     #     else:
     #         return result
+
 class Order(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
     
     DIGITAL = 'digital'
-    FISICO = 'fisico'
+    PHYSICAL = 'physical'
+    CHECKOUT = 'checkout'
+    CASH = 'cash'
 
-    DELIVERY_METHODS = ((DIGITAL, 'digital'), (FISICO, 'fisico'))
+    DELIVERY_METHODS = ((DIGITAL, 'digital'), (PHYSICAL, 'physical'))
+    PAYMENT_OPTIONS = ((CHECKOUT, 'checkout'), (CASH, 'cash'))
+
 
     delivery = models.CharField(max_length=15, choices=DELIVERY_METHODS, default=DIGITAL)
+    payment = models.CharField(max_length=15, choices=PAYMENT_OPTIONS, default=CHECKOUT)
 
     def get_cost(self):
         return self.book.price * self.quantity

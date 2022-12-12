277af7b4f1158cf250bab17df6523693691cc4a7
vicgrabru <vgravanbru@gmail.com>
2022-12-12 18:17:43 +0100

    Añadido seguimiento de pedidos

--- b00k/store/views.py
+++ b00k/store/views.py
@@ -83,6 +83,59 @@ def cartView(request):
     }
     return render(request, 'cart.html', context)
 
+def cartDetails(request, cart_id):
+    bookOrder = request.GET.get('orderBy', 'title')
+    nProducts = request.GET.get('nProducts', 25)
+    pageNumber = request.GET.get('pageNumber', 1)
+
+    cart = Cart.objects.get(id=cart_id)
+    # if request.user.is_authenticated:
+    #     username = request.user.username
+    #     cart, _ = Cart.objects.get_or_create(client=username)
+
+    #     cart.firstName = request.user.first_name
+    #     cart.lastName = request.user.last_name
+    #     cart.email = request.user.email
+        
+    #     cart.save()
+    # else:
+    #     username = placeholder
+
+    #     cart, _ = Cart.objects.get_or_create(client=placeholder)
+    #     cart.firstName = placeholder
+    #     cart.lastName = placeholder
+    #     cart.email = placeholder
+    #     cart.save()
+
+
+    
+    
+    bookList = Book.objects.filter(order__cart__id=cart_id).order_by(bookOrder)
+    
+    orderList = Order.objects.get_or_create(cart=cart)
+    paginator = Paginator(orderList, nProducts)
+    pageObj = paginator.get_page(pageNumber)
+    
+
+    totalPrice = sum([order.get_cost() for order in orderList])
+    
+
+    cart.totalPrice = totalPrice
+    cart.save()
+
+    context = {
+        'bookList': bookList,
+        'pageNumber': pageNumber,
+        'nProducts': nProducts,
+        'pageObj': pageObj,
+        'cart': cart,
+        'totalPrice': totalPrice,
+        'orderList': orderList
+
+    }
+    return render(request, 'order-details.html', context)
+
+
 def add_book(request, book_id):
     order_id = request.GET['order_id']
     order = Order.objects.get(id=order_id)

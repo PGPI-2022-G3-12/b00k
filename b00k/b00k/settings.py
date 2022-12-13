658304628d16abdd56e52175408d304d5df26d64
IJordano <n.jordano10@gmail.com>
2022-12-13 12:20:01 +0100

    feat: Add Docker config

--- b00k/b00k/settings.py
+++ b00k/b00k/settings.py
@@ -92,9 +92,6 @@ DATABASES = {
     }
 }
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
-MEDIA_URL = '/media/'
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -130,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
